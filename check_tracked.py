a56d0aa39d3ad66566b8e818c2f8a834176fb61b
Samuel Ishida <samuel.toyoshi@gmail.com>
2026-04-27 23:07:33 -0300

    chore: remove debug scripts and loose test files

--- check_tracked.py
+++
@@ -1,7 +0,0 @@
-import sqlite3
-db = sqlite3.connect('precobot.db')
-db.row_factory = sqlite3.Row
-rows = db.execute('SELECT * FROM tracked_products WHERE active=1').fetchall()
-print(f'Tracked: {len(rows)}')
-for r in rows:
-    print(dict(r))
